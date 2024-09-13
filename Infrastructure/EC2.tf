8bd24f153aea336d99fa1b8bfc868141a3b1f4b9
vikasmishraengg <vikas.f.mishra@capgemini.com>
2024-09-13 20:56:44 +0530

    network_Ec6

--- Infrastructure/EC2.tf
+++ Infrastructure/EC2.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "allow_ssh" {
+resource "aws_security_group5" "allow_ssh" {
   name        = "allow_ssh_2"
   description = "Allow SSH inbound 45"
  
@@ -21,7 +21,7 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_instance" "VikiEc" {
   ami           = "ami-0c2b8ca1dad447f8a"  # Example AMI ID; replace with a valid one for your region
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.allow_ssh.name]
+  security_groups = [aws_security_group5.allow_ssh.name]
  
   tags = {
     Name = "VikiEC5"

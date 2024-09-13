cab5512c0136c9d7bd1f9405082f48c6c4d51203
vikasmishraengg <vikas.f.mishra@capgemini.com>
2024-09-13 21:03:29 +0530

    network_Ec9

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -23,11 +23,12 @@ resource "aws_security_group" "allow_ssh" {
 }
  
 resource "aws_instance" "example" {
+  count         = 2
   ami           = "ami-0c2b8ca1dad447f8a"  # Example AMI ID; replace with a valid one for your region
   instance_type = "t2.micro"
   security_groups = [aws_security_group.allow_ssh.name]
  
   tags = {
-    Name = "TerraformExample"
+    Name = "ExampleInstance-${count.index+1}"
   }
 }

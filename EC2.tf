26d454ca2f4edf2662097d806d3a4bc0787261cb
vikasmishraengg <vikas.f.mishra@capgemini.com>
2024-09-13 20:37:33 +0530

    network_SG

---
+++ EC2.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+ 
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+ 
+resource "aws_instance" "example" {
+  ami           = "ami-0c2b8ca1dad447f8a"  # Example AMI ID; replace with a valid one for your region
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.allow_ssh.name]
+ 
+  tags = {
+    Name = "TerraformExample"
+  }
+}

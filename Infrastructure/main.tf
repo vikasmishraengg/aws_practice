0e5cd7fc9dcb01d01012a512b4d583f32c3967c2
vikasmishraengg <vikas.f.mishra@capgemini.com>
2024-09-14 00:49:21 +0530

    network_viki

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"  # Specify the AWS region
 }
- 
+
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
@@ -23,12 +23,11 @@ resource "aws_security_group" "allow_ssh" {
 }
  
 resource "aws_instance" "example" {
-  count         = 2
   ami           = "ami-0c2b8ca1dad447f8a"  # Example AMI ID; replace with a valid one for your region
   instance_type = "t2.micro"
   security_groups = [aws_security_group.allow_ssh.name]
  
   tags = {
-    Name = "ExampleInstance-${count.index+1}"
+    Name = "TerraformExample"
   }
 }

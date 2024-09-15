9f7fe9188d142da2477fb54988a7b6643cf58178
vikasmishraengg <vikas.f.mishra@capgemini.com>
2024-09-15 18:46:32 +0530

    network_cross_reference1

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,10 +1,3 @@
 provider "aws" {
   region = "us-east-1"  # Specify the AWS region
 }
-
-resource "aws_lb" "gfg_lb" {
-  name               = "gfg-network-loadbalancer"
-  internal           = false
-  load_balancer_type = "network"
-  subnets = ["subnet-0a26d92eb585e3ff0","subnet-0f3d084ff783d9348"]
-}
